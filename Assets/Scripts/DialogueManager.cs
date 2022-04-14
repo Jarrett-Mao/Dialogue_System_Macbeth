11c5f6f989ef5a43579d634fc314746786ba84ae
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2022-04-14 16:37:54 -0700

    text will display onclick

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+
     private Queue<string> sentences;
 
     // Start is called before the first frame update
@@ -11,7 +15,29 @@ public class DialogueManager : MonoBehaviour
         sentences = new Queue<string>();
     }
 
-    public void startDialogue (Dialogue dialogue){
-        Debug.Log("Starting convo with ..." + dialogue.name);
+    public void startDialogue (Dialogue dialogue){ 
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences){
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+        if (sentences.Count == 0){ 
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence; 
+    }
+
+    void EndDialogue(){
+        Debug.Log("End of conversation.");
     }
 }
